ba3ad30c6ce30e88ddd7232906b23f0b23a15827
DiasJPaullo <joaopaullodias@gmail.com>
2022-03-18 17:43:53 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Background, Titulo } from "./styles/styles"
+import styles from './styles/app.module.scss'
+function App() {
+  return (
+      <>
+        <main className={styles.main}>
+          <Titulo>Projeto Meu Repositorio</Titulo>
+        </main>
+          
+      </>
+      );
+      
+}
+
+export default App;
---
+++ src/styles/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Titulo = styled.h1`
+  font-size:px ;
+  color:#e2e2e2 ;
+`
